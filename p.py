5efaa1fdf3056b11e29f66ceeab4b4d167f69ebe
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2023-01-16 19:26:46 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,121 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import pyautogui
+import time
+
+time.sleep(4)
+
+msg1=pyautogui.prompt(text="enter your comment")
+msg2=pyautogui.prompt(text="enter your comment")
+msg3=pyautogui.prompt(text="enter your comment")
+msg4=pyautogui.prompt(text="enter your comment")
+msg5=pyautogui.prompt(text="enter your comment")
+msg6=pyautogui.prompt(text="enter your comment")
+msg7=pyautogui.prompt(text="enter your comment")
+msg8=pyautogui.prompt(text="enter your comment")
+msg9=pyautogui.prompt(text="enter your comment")
+msg10=pyautogui.prompt(text="enter your comment")
+
+
+count=0
+while count<=10:
+
+    time.sleep(5)
+
+    pyautogui.click(button="left")
+
+    pyautogui.write(msg1)
+    pyautogui.keyDown("ctrlleft")
+    pyautogui.press("enter")
+    pyautogui.keyUp("ctrlleft")
+
+    time.sleep(5)
+
+    pyautogui.click(button="left")
+
+    pyautogui.write(msg2)
+    pyautogui.keydown("ctrlleft")
+    pyautogui.press("enter")
+    pyautogui.keyUp("ctrlleft")
+
+    time.sleep(5)
+    
+    pyautogui.click(button="left")
+
+    pyautogui.write(msg3)
+    pyautogui.keydown("ctrlleft")
+    pyautogui.press("enter")
+    pyautogui.keyUp("ctrlleft")
+
+    pyautogui.click(button="left")
+
+    pyautogui.write(msg2)
+    pyautogui.keydown("ctrlleft")
+    pyautogui.press("enter")
+    pyautogui.keyUp("ctrlleft")
+
+    time.sleep(5)
+    
+    pyautogui.click(button="left")
+
+    pyautogui.write(msg2)
+    pyautogui.keydown("ctrlleft")
+    pyautogui.press("enter")
+    pyautogui.keyUp("ctrlleft")
+
+    time.sleep(5)
+   
+    pyautogui.click(button="left")
+
+    pyautogui.write(msg2)
+    pyautogui.keydown("ctrlleft")
+    pyautogui.press("enter")
+    pyautogui.keyUp("ctrlleft")
+
+    time.sleep(5)
+   
+    pyautogui.click(button="left")
+
+    pyautogui.write(msg2)
+    pyautogui.keydown("ctrlleft")
+    pyautogui.press("enter")
+    pyautogui.keyUp("ctrlleft")
+
+    time.sleep(5)
+   
+    pyautogui.click(button="left")
+
+    pyautogui.write(msg2)
+    pyautogui.keydown("ctrlleft")
+    pyautogui.press("enter")
+    pyautogui.keyUp("ctrlleft")
+
+    time.sleep(5)
+   
+    pyautogui.click(button="left")
+
+    pyautogui.write(msg2)
+    pyautogui.keydown("ctrlleft")
+    pyautogui.press("enter")
+    pyautogui.keyUp("ctrlleft")
+
+    time.sleep(5)
+   
+    pyautogui.click(button="left")
+
+    pyautogui.write(msg2)
+    pyautogui.keydown("ctrlleft")
+    pyautogui.press("enter")
+    pyautogui.keyUp("ctrlleft")
+
+    time.sleep(15)
+     
+    
+
+
+
+
+
